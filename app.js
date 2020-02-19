8a6b100f90931d95854a0e827bb42c5c5b8e184a
Igor Rogov <kanva777@gmail.com>
2020-02-19 16:43:27 +0300

    Final

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const fulldate = new Date(); 
-const hour = fulldate.getHours();
-const min = fulldate.getMinutes();
-const sec = fulldate.getSeconds();
+function clock() {
+  const fulldate = new Date();
+  var hour = fulldate.getHours();
+  var min = fulldate.getMinutes();
+  var sec = fulldate.getSeconds();
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("minute").innerHTML = ":" + min;
+  document.getElementById("second").innerHTML = ":" + sec;
+}
+
+setInterval(clock, 100);
